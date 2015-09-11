2dd16c59d258fd7e101ab05084edeb907e489698
flynsarmy <flynsarmy@gmail.com>
2015-09-11 14:47:54 +1000

    Make it go

--- Plugin.php
+++ Plugin.php
@@ -1,6 +1,9 @@
 <?php namespace Nsrosenqvist\ThemesPlus;
 
-use Nsrosenqvist\ThemesPlus\Classes\ThemePluginManager;
+use App;
+use File;
+use Cms\Classes\Theme;
+use System\Classes\ComposerManager;
 
 class Plugin extends \System\Classes\PluginBase
 {
@@ -17,7 +20,30 @@ class Plugin extends \System\Classes\PluginBase
 
     public function boot()
     {
-        $themePluginManager = new ThemePluginManager();
-        $themePluginManager->init();
+        // Get active theme's directory
+        $theme = Theme::getActiveTheme();
+        $themePath = $theme->getPath();
+        $vendorPath = $themePath.'/vendor';
+        $providerPath = $themePath.'/Theme.php';
+
+        // Autoload active theme's vendor directory
+        if ( File::isDirectory($vendorPath) )
+            ComposerManager::instance()->autoload($vendorPath);
+
+        // Load your theme's Theme.php file as a service provider
+        if ( File::exists($providerPath) )
+        {
+            require $providerPath;
+
+            if ( class_exists('ThemesPlusTheme\\Theme'))
+            {
+                \App::register('\\ThemesPlusTheme\\Theme');
+
+                // Boot our theme's service provider
+                //$instance = new \ThemesPlusTheme\Theme(App::make('app'));
+                //$instance->boot();
+            }
+        }
+
     }
 }
---
+++ README.md
@@ -0,0 +1,35 @@
+# Description
+
+This plugin allows execution of complex code directly from plugins by providing
+a service provider and composer support. Through the new service provider, 
+anything that can be done with a plugin can now be done with a theme.
+
+## Installation
+
+* `git clone` to */plugins/nsrosenqvist/themesplug* directory
+* `php artisan plugin:refresh Nsrosenqvist.ThemesPlus`
+* In your active theme's directory add a *Theme.php* file with the following code:
+
+
+    
+    <?php namespace ThemesPlusTheme;
+    
+    class Theme extends \System\Classes\PluginBase
+    {
+        public function pluginDetails()
+        {
+            return [
+                'name' => 'Themes+Theme',
+                'description' => 'Execute complex tasks from your theme.',
+                'author' => 'Your Name',
+                'icon' => 'icon-leaf',
+                'homepage' => 'https://www.yourite.com/'
+            ];
+        }
+    
+        public function boot()
+        {
+            // your code here
+        }
+    }
+    
